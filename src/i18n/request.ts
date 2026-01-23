641885791f0c12f98605065f9ba9bac7aeeef37b
Majit Morosetti <majit.morosetti1996@gmail.com>
2026-01-23 16:07:09 +0100

    added intl routing, [locale] dir architecture and basic layouts

---
+++ src/i18n/request.ts
@@ -0,0 +1,15 @@
+import { getRequestConfig } from "next-intl/server";
+import { routing } from "./routing";
+
+export default getRequestConfig(async ({ requestLocale }) => {
+    let locale = await requestLocale;
+
+    if (!locale || !routing.locales.includes(locale as 'fr' | 'en')) { // Check if locale is empty or does not match 'fr' or 'en'
+        locale = routing.defaultLocale; //falls back to 'fr' as default locale
+    }
+
+    return {
+        locale,
+        messages: (await import(`../messages/${locale}.json`)).default // load matching language here, next optimizes dynamic imports in returns
+    };
+})
